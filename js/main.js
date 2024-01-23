61a2e2e880a2921462770245c15723527a4dda2f
Memai2023 <maria.hong26@gmail.com>
2024-01-23 13:17:28 +0100

    erased navmeny in html and put in JS, modified the order of script in html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(() => {
-
-
-
+    $(document).ready(function () {
+        function createNavbar() {
+            $(".nav-menu").append(`
+                <ul>
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="./about.html">About</a></li>
+                    <li><a href="./students.html">Students</a></li>
+                    <li><a href="./companies.html">Companies</a></li>
+                </ul>`);
+        }
+    
+        createNavbar();
+    });
 });
