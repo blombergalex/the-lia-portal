2224a5eafbb73b3598bcf18f41934b90687498dc
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-01-23 13:27:13 +0100

    styling, add media query

--- js/students.js
+++ js/students.js
@@ -22,7 +22,7 @@ $(() => {
                     <div class="${this.name} profile-large">
                         <button id="cancel-btn">&#10006;</button> 
                         <img class="profile-img" src="../images/${this.name}.png" height=100px width=100px alt="image of ${this.name}"></img>
-                        <h3>${this.name}</h3>
+                        <h3 class="name">${this.name}</h3>
                         <h4>${this.title}</h4>
                         <p>${this.description}</p>
                         <p class="student-skills">${this.skills}</p>
