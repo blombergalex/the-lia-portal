d3253e6ed0e41a6818b4494d363a3311bcd032b3
Memai2023 <maria.hong26@gmail.com>
2024-01-23 22:56:41 +0100

    Hamburger menu, Navigation menu, added svg icons, create menu in js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 $(() => {
-    $(document).ready(function () {
-        function createNavbar() {
-            $(".nav-menu").append(`
-                <ul>
-                    <li><a href="./index.html">Home</a></li>
-                    <li><a href="./about.html">About</a></li>
-                    <li><a href="./students.html">Students</a></li>
-                    <li><a href="./companies.html">Companies</a></li>
-                </ul>`);
+
+    const createMenu = (className) => {
+        let menuContent = `
+        <li ${className === '.navbar' ? ' class="viewOnDesktop"' : ''}><a href="index.html">Home</a></li>
+        <li ${className === '.navbar' ? ' class="viewOnDesktop"' : ''}><a href="about.html">About</a></li>
+        <li ${className === '.navbar' ? ' class="viewOnDesktop"' : ''}><a href="students.html">Students</a></li>
+        <li ${className === '.navbar' ? ' class="viewOnDesktop"' : ''}><a href="companies.html">Companies</a></li>`;
+
+        if (className === ".sidebar") {
+            menuContent = `<li class="closebutton" onclick=hideSidebar()><a href="#"><img src="./images/closecross.svg"></a></li>` + menuContent;
+        } else if (className === ".navbar") {
+            menuContent = `<li><a href="#"><img class="logo" src="./images/lia-logo2.png"></a></li>` + menuContent + `<li class="menubutton" onclick=showSidebar()><a href="#"><img src="./images/burgermenu.svg"></a></li>`;
         }
-    
-        createNavbar();
-    });
+
+        $(className).append(menuContent);
+    }
+
+    $(document).ready(function () {
+        createMenu(".sidebar");
+        createMenu(".navbar")
+    })
+
+    window.showSidebar = function showSidebar() {
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'flex'  }
+
+    window.hideSidebar = function hideSidebar() {
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'none'  }
+   
+
 });
