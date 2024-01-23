6961dd5feefe86cab71151b77a7f0a70c45059de
eri.mari <erimarilin@outlook.com>
2024-01-23 07:58:12 +0100

    start object

--- js/students.js
+++ js/students.js
@@ -1,5 +1,10 @@
 $(() => {
-   
-    
+    let student = []
+    function students(title, name, description, skills) {
+        this.title = title;
+        this.name = name;
+        this.description = description;
+        this.skills = skills; 
+    }
     
 });
