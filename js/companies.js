319bbd43b29fa7044539931c2f60df2930363e4f
Darius K <darius.kaya@gmail.com>
2024-01-23 12:05:51 +0100

    add popup company page

--- js/companies.js
+++ js/companies.js
@@ -151,17 +151,23 @@ $(() => {
         $(".overlay").toggle();
     })
 
+    $(".main-content").on("click", ".cancel-btn", () => {
+        $(".popup").toggle();
+        $(".overlay").toggle();
+    })
+
     const popup = company => {
         $(companyArray).each((index, y) => {
             if (y.name === company) {
                 $(".popup").empty().append(
                     `
+                    <button class="cancel-btn">&#10006;</button>
                     <img class="popup-image" src="${y.image}">
                     <h2>${y.name}</h2>
                     <p><span>Office:</span> ${y.location}</p>
                     <p>${y.number}</p>
                     <p>${y.email}</p>
-                    <p>${y.website}</p>
+                    <a href="${y.website}">${y.website}</a>
                     <p><span>About Us:</span> ${y.aboutUs}</p>
                     <p><span>What to expect:</span> ${y.weCanOffer}</p>
                     <p><span>Required skills:</span> ${y.requiredSkills}</p>
