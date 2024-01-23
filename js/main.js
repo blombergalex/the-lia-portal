8ef06f118d36ff4a05335a9513f0d12231be5d74
Darius K <darius.kaya@gmail.com>
2024-01-23 21:07:19 +0100

    fix dynamic about page

--- js/main.js
+++ js/main.js
@@ -4,7 +4,16 @@ $(() => {
             $(".nav-menu").append(`
                 <ul>
                     <li><a href="./index.html">Home</a></li>
-                    <li><a href="./about.html">About</a></li>
+                    <li><a href="./about.html">
+                    About
+                        <ul>
+                            <li><a href="about.html?name=Alex Blomberg">Alex</a></li>
+                            <li><a href="about.html?name=Erika Lundström">Erika</a></li>
+                            <li><a href="about.html?name=Shada Bazger">Shada</a></li>
+                            <li><a href="about.html?name=Maria Hendricks">Maria</a></li>
+                            <li><a href="about.html?name=Darius Kaya">Darius</a></li>
+                        </ul>
+                    </a></li>
                     <li><a href="./students.html">Students</a></li>
                     <li><a href="./companies.html">Companies</a></li>
                 </ul>`);
@@ -12,4 +21,5 @@ $(() => {
     
         createNavbar();
     });
+
 });
