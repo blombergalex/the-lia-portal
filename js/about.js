95a8fd0373816a7df9f1d15b194726a41446e989
eri.mari <erimarilin@outlook.com>
2024-01-24 13:24:54 +0100

    added info me

--- js/about.js
+++ js/about.js
@@ -52,10 +52,10 @@ $(() => {
     let erika = new Coder(
         "./images/cat.jpg",
         "Erika Lundström",
-        "email",
-        "favoritmat",
-        "favoritdjur",
-        "några meningar om dig själv"
+        "erimarilin@outlook.com",
+        "Home cooked food!",
+        "Do we have to pick one? I like most animals except silver fiskar and tipuloidea!",
+        "This will be hard, im often opposite to my self, Im forgetful, even tho i hate forget things, but i can have excellent memories about something I heard or did 10 years ago. I love to do spontaneous things, but i dont like when things never be as expected. I think my strong side is I love to learn new things, dig up information, to discuss why/how things are as they are, and figure out how to solve things and brainstorm in my head and with others, after solutions, Im never out of ideas!"
     );
 
     let alex = new Coder(
