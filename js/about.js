ac8dc5a007ae365fc874a6e1139f80959c396001
Darius K <darius.kaya@gmail.com>
2024-01-24 07:50:03 +0100

    change email

--- js/about.js
+++ js/about.js
@@ -25,7 +25,7 @@ $(() => {
     let darius = new Coder(
         "./images/cat.jpg",
         "Darius Kaya",
-        "darius@kaya.au",
+        "darius@kaya.com",
         "Good food",
         "Rhino",
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad corrupti incidunt voluptas magni voluptatem eum, accusantium suscipit fuga sequi laborum minima voluptates maiores sit at sed dolorem temporibus necessitatibus unde deleniti corporis. Nam voluptatibus consequuntur quasi magni error nostrum illo incidunt. Itaque ipsa ab blanditiis. Eius itaque similique mollitia dolorem voluptate dolore animi inventore consequuntur? Tempore laboriosam dolorem porro quis dolore beatae deserunt saepe nobis? Nemo deserunt nisi adipisci quia quae obcaecati numquam quas qui, assumenda fuga temporibus. Tempore ab quis doloremque sit, quo eius similique dolore nemo facilis voluptas voluptatibus quos recusandae adipisci corporis ratione maiores saepe atque optio."
