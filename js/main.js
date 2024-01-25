241b8ded3baddf7e44e71dc7be8af59715f37c6d
Memai2023 <maria.hong26@gmail.com>
2024-01-25 17:45:32 +0100

    fixed the submenu mobile in css

--- js/main.js
+++ js/main.js
@@ -3,22 +3,27 @@ $(() => {
     const createMenu = (className) => {
         let menuContent = `
         <li ${className === '.navbar' ? ' class="viewOnDesktop"' : ''}><a href="index.html">Home</a></li>
-        <li ${className === '.navbar' ? ' class="viewOnDesktop"' : ''}><a href="about.html">About ▼</a>
-            <ul class="submenu">
-                <li><a href="about.html?name=Alex Blomberg">Alex</a></li>
-                <li><a href="about.html?name=Darius Kaya">Darius</a></li>
-                <li><a href="about.html?name=Erika Lundström">Erika</a></li>
-                <li><a href="about.html?name=Shada Bazger">Shada</a></li>
-                <li><a href="about.html?name=Maria Hendricks">Maria</a></li>
-            </ul>
-        </li>
+
         <li ${className === '.navbar' ? ' class="viewOnDesktop"' : ''}><a href="students.html">Students</a></li>
-        <li ${className === '.navbar' ? ' class="viewOnDesktop"' : ''}><a href="companies.html">Companies</a></li>`;
+        <li ${className === '.navbar' ? ' class="viewOnDesktop"' : ''}><a href="companies.html">Companies</a></li>
+        <li ${className === '.navbar' ? ' class="viewOnDesktop"' : ''}><a href="#">About ▼</a>
+        <ul class="submenu">
+            <li><a href="about.html">LIA Portalen</a></li>
+            <li><a href="about.html?name=Darius Kaya">Darius</a></li>
+            <li><a href="about.html?name=Alex Blomberg">Alexandra</a></li>
+            <li><a href="about.html?name=Erika Lundström">Erika</a></li>
+            <li><a href="about.html?name=Shada Bazger">Shada</a></li>
+            <li><a href="about.html?name=Maria Hendricks">Maria</a></li>
+        </ul>
+    </li>
+        
+        
+        `;
 
         if (className === ".sidebar") {
             menuContent = `<li class="closebutton" onclick=hideSidebar()><a href="#"><img src="./images/closecross.svg"></a></li>` + menuContent;
         } else if (className === ".navbar") {
-            menuContent = `<li><a href="#"><img class="logo" src="./images/lia-logo2.png"></a></li>` + menuContent + `<li class="menubutton" onclick=showSidebar()><a href="#"><img src="./images/burgermenu.svg"></a></li>`;
+            menuContent = `<li><img class="logo" src="./images/lia-logo2.png"></li>` + menuContent + `<li class="menubutton" onclick=showSidebar()><a href="#"><img src="./images/burgermenu.svg"></a></li>`;
         }
 
         $(className).append(menuContent);
